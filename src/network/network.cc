90ce95bc33df04e2ebaff573485e2dce3d90013d
John Hood <cgull@glup.org>
2016-04-12 03:52:49 +0000

    Workarounds for Windows Subsystem for Linux.

--- src/network/network.cc
+++ src/network/network.cc
@@ -157,10 +157,9 @@ Connection::Socket::Socket( int family )
 
   /* Disable path MTU discovery */
 #ifdef HAVE_IP_MTU_DISCOVER
-  char flag = IP_PMTUDISC_DONT;
-  socklen_t optlen = sizeof( flag );
-  if ( setsockopt( _fd, IPPROTO_IP, IP_MTU_DISCOVER, &flag, optlen ) < 0 ) {
-    throw NetworkException( "setsockopt", errno );
+  int flag = IP_PMTUDISC_DONT;
+  if ( setsockopt( _fd, IPPROTO_IP, IP_MTU_DISCOVER, &flag, sizeof flag ) < 0 ) {
+    // throw NetworkException( "setsockopt", errno );
   }
 #endif
 
@@ -472,22 +471,31 @@ string Connection::recv_one( int sock_to_recv, bool nonblocking )
 
   /* receive source address */
   header.msg_name = &packet_remote_addr.sa;
-  header.msg_namelen = sizeof( packet_remote_addr );
+  header.msg_namelen = sizeof packet_remote_addr.sa;
 
   /* receive payload */
   msg_iovec.iov_base = msg_payload;
-  msg_iovec.iov_len = Session::RECEIVE_MTU;
+  msg_iovec.iov_len = sizeof msg_payload;
   header.msg_iov = &msg_iovec;
   header.msg_iovlen = 1;
 
   /* receive explicit congestion notification */
   header.msg_control = msg_control;
-  header.msg_controllen = Session::RECEIVE_MTU;
+  header.msg_controllen = sizeof msg_control;
 
   /* receive flags */
   header.msg_flags = 0;
 
   ssize_t received_len = recvmsg( sock_to_recv, &header, nonblocking ? MSG_DONTWAIT : 0 );
+  // Workaround for Windows Subsystem for Linux.
+  if ( received_len < 0 && errno == EINVAL ) {
+    socklen_t sock_size = header.msg_namelen;
+    received_len = recvfrom( sock_to_recv, msg_payload, Session::RECEIVE_MTU, nonblocking ? MSG_DONTWAIT : 0,
+      &packet_remote_addr.sa, &sock_size);
+    header.msg_namelen = sock_size;
+    header.msg_flags = 0;
+    header.msg_controllen = 0;
+  }
 
   if ( received_len < 0 ) {
     throw NetworkException( "recvmsg", errno );
