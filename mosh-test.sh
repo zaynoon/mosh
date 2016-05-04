1a9c029ea75070265e763e95f934af7fb311d2b9
John Hood <cgull@glup.org>
2016-05-04 05:47:56 +0000

    update paths

--- mosh-test.sh
+++ mosh-test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-LD_PRELOAD=$PWD/forkpty.so strace -qqTttffo mosh.strace /home/cgull/src/mosh-keithw/scripts/mosh --client=/home/cgull/src/mosh-keithw/src/frontend/mosh-client --server=/home/cgull/src/mosh-keithw/src/frontend/mosh-server --local 127.0.0.1
+LD_PRELOAD=/home/cgull/src/wsl-forkpty-shim/forkpty.so strace -qqTttffo mosh.strace /home/cgull/src/mosh-keithw/scripts/mosh --client=/home/cgull/src/mosh-keithw/src/frontend/mosh-client --server=/home/cgull/src/mosh-keithw/src/frontend/mosh-server --local 127.0.0.1
 
