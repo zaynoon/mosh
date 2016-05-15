45fe712ddbedf6ba15030bd9f87090b9897e84ad
john hood <cgull@glup.org>
2016-05-15 02:39:43 -0400

    moshd2.py: UDP and TCP listeners work

--- moshd2.py
+++ moshd2.py
@@ -6,44 +6,53 @@ import socket
 import sys
 
 from twisted.python.log import startLogging
+from twisted.internet.protocol import DatagramProtocol, Factory
+from twisted.protocols.basic import Int32StringReceiver
 from twisted.internet import reactor
-from twisted.internet.protocol import Protocol, DatagramProtocol
 
-class SomeUDP(DatagramProtocol):
+
+class TcpDgram(Int32StringReceiver):
+    def __init__(self, remoteDict):
+        self.remoteDict = remoteDict
+
+    def stringReceived(self, string):
+        print(u"Got a message of {} bytes.".format(len(string)))
+
+    def sendStringV(self, string):
+        self.sendString(string)
+        print(u"Sent a message of {} bytes.".format(len(string)))
+        
+class TcpFactory(Factory):
+    def __init__(self):
+        self.remoteDict = {} # maps remote addresses to mosh-server connection
+
+    def buildProtocol(self, addr):
+        return TcpDgram(self.remoteDict)
+
+
+class UdpDgram(DatagramProtocol):
     def datagramReceived(self, datagram, (host, port)):
         print(u"Got a datagram of {} bytes from {}:{}.".format(len(datagram), host, port))
-
-    def sendFoo(self, foo, ip, port):
         self.transport.write(
-            (u"Foo datagram: {}".format(foo)).encode("utf-8"),
-            (ip, port))
+            datagram,
+            (host, port))
 
-class SomeSender(object):
-    def __init__(self, proto):
-        self.proto = proto
-
-    def start(self, v4):
-        self.v4 = v4
-        reactor.callLater(3, self._send)
-
-    def _send(self):
-        if self.v4:
-            self.proto.sendFoo(u"v4", b"127.0.0.1", 12345)
-        else:
-            self.proto.sendFoo(u"Hello or whatever", b"::1", 12345)
-        self.start(self.v4)
-
+    def sendDatagram(self, datagram, host, port):
+        self.transport.write(
+            datagram,
+            (host, port))
+        print(u"Sent a datagram of {} bytes to {}:{}.".format(len(datagram), host, port))
 
 if __name__ == "__main__":
 
     startLogging(sys.stdout)
 
-    proto = SomeUDP()
-    protov6 = SomeUDP()
-    reactor.listenUDP(12345, proto)
-    reactor.listenUDP(12345, protov6, interface="::")
+    proto = UdpDgram()
+    protov6 = UdpDgram()
+    # reactor.listenUDP(60001, proto)
+    reactor.listenUDP(60001, protov6, interface="::")
 
-    SomeSender(proto).start(True)
-    SomeSender(protov6).start(False)
+    # reactor.listenTCP(60001, TcpFactory())
+    reactor.listenTCP(60001, TcpFactory(), interface="::")
 
     reactor.run()
