d1b1f57eec4b27e461248204c68bbbf62a1bde8d
John Hood <cgull@glup.org>
2016-11-03 01:13:46 -0400

    Split Travis, packaging-dependent, and OS X scripting.  Add reporting.

---
+++ macosx/osx-xcode.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# OS X and Xcode support script.
+#
+
+#
+# Describe the OS X and Xcode installation, patches, etc as best as possible.
+#
+describe()
+{
+    printf "OS X packages:\n"
+    pkgutil --pkgs
+    printf "Xcode version:\n"
+    xcodebuild -version
+    printf "CLT version:\n"
+    pkgutil --pkg-info=com.apple.pkg.CLTools_Executables
+    printf "Xcode path:\n"
+    xcode-select --print-path
+    # System Profiler's XML can be read more easily with plutil -p, or
+    # saved with an .spx extension and opend with the System Profiler
+    # GUI.
+    printf "System Profiler:\n"
+    system_profiler -xml 2>/dev/null
+    printf "OS X report end.\n"
+}
+
+#
+# Do something.
+#
+set -e
+"$@"
